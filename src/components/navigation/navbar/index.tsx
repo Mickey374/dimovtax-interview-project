7c16ef427492cb86de35236883f6d427127f34fc
Mickey374 <asantemichael37483@gmail.com>
2026-06-27 04:02:00 +0200

    feat(ui): implemented toggle for theme on navbar

--- src/components/navigation/navbar/index.tsx
+++ src/components/navigation/navbar/index.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import ThemePicker from "./ThemePicker";
 
 const Navbar = () => {
   return (
@@ -15,8 +15,8 @@ const Navbar = () => {
 
       <p>Global Search here...</p>
 
-      <div className="flex-between gap-2">
-        <p>Theme icon</p>
+      <div className="flex-between gap-5">
+        <ThemePicker />
         <p>Profile icon</p>
       </div>
     </nav>
--- src/context/Theme.tsx
+++ src/context/Theme.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { ThemeProviderProps, ThemeProvider as NextThemesProvider } from "next-themes";
-import React from "react";
 
 const ThemeProvider = ({ children, ...props }: ThemeProviderProps) => {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
